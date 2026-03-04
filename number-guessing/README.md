44f7a91067779207cfb97ff9c59624de2bcd37a4
darai-jiwan <jiwandarai101@gmail.com>
2026-03-04 19:43:35 +0545

    Initial commit

---
+++ number-guessing/README.md
@@ -0,0 +1,2 @@
+# number-guessing
+This is a simple game of guessing a number where a player have to guess a number within a limit chance.
---
+++ number-guessing/numberguessing.py
@@ -0,0 +1,70 @@
+import random 
+
+game = True 
+while game == True :
+    level = input("choose your level (easy/medium /hard): ").lower()
+    if level == "easy":
+        count = 5
+        number = random.randint(1,20)
+        while count >0:
+            guess = int (input("Guess a number between 1-20:"))
+            if number == guess:
+                print("You are Correct")
+                game = False
+                break
+            elif guess< number:
+                print("higher")
+                count -=1
+            elif guess > number:
+                print("lower")
+                count -=1
+
+            else: 
+                print("Enter a interger value")
+        print(f"The correct number is {number}")
+
+    elif level =="medium":
+        count = 7
+        number = random.randint(1,50)
+        while count >0:
+            guess = int (input("Guess a number between 1-50:"))
+            if number == guess:
+                print("You are Correct")
+                game = False
+                break
+            elif guess< number:
+                print("higher")
+                count -=1
+            elif guess > number:
+                print("lower")
+                count -=1
+
+
+            else: 
+                print("Enter a interger value")
+        print(f"The correct number is {number}")
+
+
+    elif level =="hard":
+        count = 10
+        number = random.randint(1,100)
+        while count > 0:
+            guess = int (input("Guess a number between 1-100:"))
+            if number == guess:
+                print("You are Correct")
+                game = False
+                break
+            elif guess< number:
+                print("higher")
+                count -=1
+            elif guess > number:
+                print("lower")
+                count -=1
+
+            else: 
+                print("Enter a interger value")
+        print(f"The correct number is {number}")
+
+    else: 
+        print("Choose a correct option (easy/ medium/ hard)")
+
